b7a4a5a7327942392f3ecfe70b01bb455bcbc735
Muhammad Yusuf Firdaus <yusuffirdaus1999@gmail.com>
2021-03-08 21:35:11 +0700

    Menambahkan data dummy

---
+++ app/src/main/java/org/d3if4134/galeryhewan/Hewan.kt
@@ -0,0 +1,6 @@
+package org.d3if4134.galeryhewan
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String
+)
--- app/src/main/java/org/d3if4134/galeryhewan/MainActivity.kt
+++ app/src/main/java/org/d3if4134/galeryhewan/MainActivity.kt
@@ -2,10 +2,29 @@ package org.d3if4134.galeryhewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Jumlah data: " + getData().size)
+
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+                Hewan("Angsa", "Cygnus olor"),
+                Hewan("Ayam", "Gallus gallus"),
+                Hewan("Bebek", "Cairina moschata"),
+                Hewan("Domba", "Ovis ammon"),
+                Hewan("Kalkun", "Meleagris gallopavo"),
+                Hewan("Kambing", "Capricornis sumatrensis"),
+                Hewan("Kelinci", "Oryctolagus cuniculus"),
+                Hewan("Kerbau", "Bubalus bubalis"),
+                Hewan("Kuda", "Equus caballus"),
+                Hewan("Sapi", "Bos taurus"),
+                )
     }
 }
